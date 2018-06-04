8e23cec7c030c7b717785167755df0cb3bd20794
rarspace01 <rarspace01@gmail.com>
2018-06-04 21:54:48 +0200

    updates to spring 2, re-enables params and introduces spring scheduler

--- src/main/java/hello/Application.java
+++ src/main/java/hello/Application.java
@@ -1,12 +1,14 @@
 package hello;
 
-import java.util.Arrays;
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
+import java.util.Arrays;
 
 @SpringBootApplication
+@EnableScheduling
 public class Application {
 
     public static void main(String[] args) {
---
+++ src/main/java/hello/BoatController.java
@@ -0,0 +1,47 @@
+package hello;
+
+import kotlin.jvm.internal.Intrinsics;
+import kotlin.text.Charsets;
+import org.jetbrains.annotations.NotNull;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import torrent.PirateBay;
+import torrent.Premiumize;
+import torrent.Torrent;
+
+import java.util.Base64;
+import java.util.List;
+
+@RestController
+public final class BoatController {
+    @GetMapping({"/"})
+    @NotNull
+    public final String index() {
+        return "Greetings from Spring Boot!";
+    }
+
+    @GetMapping({"/boat"})
+    @NotNull
+    public final String getTorrents(@RequestParam("q") @NotNull String searchString) {
+        Intrinsics.checkParameterIsNotNull(searchString, "searchString");
+        PirateBay pirateBay = new PirateBay();
+        List resultList = pirateBay.searchTorrents(searchString);
+        return "G: " + resultList.subList(0, Math.min(resultList.size(), 10));
+    }
+
+    @GetMapping({"/boat/download"})
+    @NotNull
+    public final String downloadTorrent(@RequestParam("d") @NotNull String downloadUri) {
+        Intrinsics.checkParameterIsNotNull(downloadUri, "downloadUri");
+        byte[] var10000 = Base64.getUrlDecoder().decode(downloadUri);
+        Intrinsics.checkExpressionValueIsNotNull(var10000, "Base64.getUrlDecoder().decode(downloadUri)");
+        byte[] var3 = var10000;
+        String decodedUri = new String(var3, Charsets.UTF_8);
+        Torrent torrentToBeDownloaded = new Torrent();
+        torrentToBeDownloaded.magnetUri = decodedUri.toString();
+        String var4 = (new Premiumize()).addTorrentToQueue(torrentToBeDownloaded);
+        Intrinsics.checkExpressionValueIsNotNull(var4, "Premiumize().addTorrentT…ue(torrentToBeDownloaded)");
+        return var4;
+    }
+}
