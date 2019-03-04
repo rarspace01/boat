5f271e0a828996702984d02d4c527f68cebd39e9
rarspace01 <rarspace01@users.noreply.github.com>
2019-03-04 08:27:58 +0100

    typo

--- runPirateboat.sh
+++ runPirateboat.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 while :; do
     wget -O pirateboat.jar https://github.com/rarspace01/pirateboat/releases/latest/download/pirateboat.jar
-    chmod +x piratebay.jar
-    java -jar piratebay.jar
+    chmod +x pirateboat.jar
+    java -jar pirateboat.jar
 done
