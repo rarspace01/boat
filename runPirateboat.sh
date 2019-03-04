6f6c67c26cedb6a7c82dbdf27f059f4fc413200c
Denis Hamann <40381127+denishamann1337@users.noreply.github.com>
2019-03-04 08:20:22 +0100

    Create runPirateboat.sh

---
+++ runPirateboat.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+while :; do
+    wget -O pirateboat.jar https://github.com/rarspace01/pirateboat/releases/latest/download/pirateboat.jar
+    chmod +x piratebay.jar
+    java -jar piratebay.jar
+done
