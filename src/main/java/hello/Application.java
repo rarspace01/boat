112c339d8d5e01b407a28e6adfd8d7cbd896cd51
rarspace01 <rarspace01@gmail.com>
2019-08-16 15:28:17 +0200

    fixes bean configuration error

--- src/main/java/hello/Application.java
+++ src/main/java/hello/Application.java
@@ -4,6 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 import java.util.Arrays;
@@ -11,6 +12,7 @@ import java.util.Arrays;
 @SpringBootApplication
 @EnableAutoConfiguration
 @EnableScheduling
+@ComponentScan({"hello","torrent"})
 public class Application {
 
     public static void main(String[] args) {
