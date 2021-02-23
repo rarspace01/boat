409f8325fc36883661885772f93cbf4190082203
rarspace01 <rarspace01@gmail.com>
2021-02-23 15:36:02 +0100

    fix imports in tests

--- src/test/java/pirateboat/info/TheFilmDataBaseServiceTest.java
+++ src/test/java/pirateboat/info/TheFilmDataBaseServiceTest.java
@@ -7,8 +7,9 @@ import org.junit.jupiter.api.Test;
 
 import java.util.List;
 
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 
 class TheFilmDataBaseServiceTest {
 
