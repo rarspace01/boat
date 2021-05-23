8ccfc27116fe18ccf858301f3878c65076c035e3
rarspace01 <rarspace01@gmail.com>
2021-05-23 16:53:47 +0200

    move test

--- src/test/java/boat/torrent/TorrentSearchEngineServiceTest.java
+++ src/test/java/boat/torrent/TorrentSearchEngineServiceTest.java
@@ -1,27 +1,26 @@
-package boat;
+package boat.torrent;
 
 import java.util.List;
 
-import boat.torrent.Torrent;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-class BoatControllerTest {
+class TorrentSearchEngineServiceTest {
 
     @Test
     void cleanDuplicates() {
         // Given
-        final BoatController boatController = new BoatController(null, null, null, null, null, null, null, null);
+        final TorrentSearchEngineService tse = new TorrentSearchEngineService(null, null);
 
         final Torrent s1 = new Torrent("S1");
         final Torrent s2 = new Torrent("S2");
-        s1.magnetUri="btih:ABC&";
-        s2.magnetUri="btih:ABC&";
+        s1.magnetUri = "btih:ABC&";
+        s2.magnetUri = "btih:ABC&";
         s1.searchRating = 1;
         s2.searchRating = 2;
         // When
-        final List<Torrent> torrentList = boatController.cleanDuplicates(List.of(s1, s2));
+        final List<Torrent> torrentList = tse.cleanDuplicates(List.of(s1, s2));
         // Then
         assertThat(torrentList.get(0)).isEqualTo(s2);
         assertThat(torrentList.size()).isEqualTo(1);
