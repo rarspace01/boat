6814f8aa3e88ab24611813a3d13d1a8d4d00e53c
rarspace01 <rarspace01@gmail.com>
2021-05-25 12:08:36 +0200

    improve MediaItem toString

--- src/main/java/boat/info/MediaItem.kt
+++ src/main/java/boat/info/MediaItem.kt
@@ -5,4 +5,13 @@ data class MediaItem(
     var originalTitle: String? = null,
     val year: Int? = null,
     var type: MediaType? = null
-)
+) {
+    override fun toString(): String {
+        return StringBuilder().append("[$title]")
+            .append(if (originalTitle != null) "[${originalTitle}]" else "")
+            .append(if (year != null) "[$year]" else "")
+            .append(if (type != null) "[$type]" else "")
+            .append("\n")
+            .toString()
+    }
+}
