41381245f9a50d754007a3c31a402671c4d9e1d1
rarspace01 <rarspace01@gmail.com>
2021-09-29 12:02:30 +0200

    update docker setup

--- runDockeredBoat.sh
+++ runDockeredBoat.sh
@@ -1,2 +1,12 @@
 #!/bin/bash
-docker build . -t rarspace01/boat && docker run --restart always -d -it -p 8080:8080 rarspace01/boat
+#if [ `uname -m` -eq $zero ]; then
+case $(uname -m) in
+"x86_64" | "i386" | "i686")
+  echo "Intel like CPU, use normal Dockerfile"
+  docker build . -t rarspace01/boat && docker run --restart always -d -it -p 8080:8080 rarspace01/boat
+  ;;
+  *)
+  echo "Non intel cpu - use dynamic dockerfile"
+  docker build -f nonintel.Dockerfile . -t rarspace01/boat && docker run --restart always -d -it -p 8080:8080 rarspace01/boat
+  ;;
+esac
