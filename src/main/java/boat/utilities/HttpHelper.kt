19593a23060956acc0509afff9c2210d6dbe2e8e
rarspace01 <rarspace01@gmail.com>
2022-03-18 22:43:55 +0100

    transform http helper

--- src/main/java/boat/utilities/HttpHelper.kt
+++ src/main/java/boat/utilities/HttpHelper.kt
@@ -1,214 +1,205 @@
-package boat.utilities;
+package boat.utilities
 
-import java.io.BufferedReader;
-import java.io.File;
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.net.URL;
-import java.net.URLConnection;
-import java.nio.channels.Channels;
-import java.nio.channels.ReadableByteChannel;
-import java.security.KeyManagementException;
-import java.security.NoSuchAlgorithmException;
-import java.util.List;
-import java.util.concurrent.TimeUnit;
-
-import javax.net.ssl.HttpsURLConnection;
-import javax.net.ssl.SSLContext;
-import javax.net.ssl.TrustManager;
-import javax.net.ssl.X509TrustManager;
-
-import org.springframework.stereotype.Service;
-
-import org.apache.commons.io.IOUtils;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import org.apache.commons.io.IOUtils
+import org.slf4j.LoggerFactory
+import org.springframework.stereotype.Service
+import java.io.BufferedReader
+import java.io.File
+import java.io.FileOutputStream
+import java.io.IOException
+import java.io.InputStreamReader
+import java.net.URL
+import java.net.URLConnection
+import java.nio.channels.Channels
+import java.security.KeyManagementException
+import java.security.NoSuchAlgorithmException
+import java.security.cert.X509Certificate
+import java.util.concurrent.TimeUnit
+import javax.net.ssl.HttpsURLConnection
+import javax.net.ssl.SSLContext
+import javax.net.ssl.TrustManager
+import javax.net.ssl.X509TrustManager
 
 @Service
-public class HttpHelper {
-
-    private static String charset = "UTF-8";  // Or in Java 7 and later, use the constant: java.nio.charset.StandardCharsets.UTF_8.name()
-    private static final Logger log = LoggerFactory.getLogger(HttpHelper.class);
-    private static TrustManager[] trustAllCerts = new TrustManager[]{
-        new X509TrustManager() {
-            public java.security.cert.X509Certificate[] getAcceptedIssuers() {
-                return null;
-            }
-
-            public void checkClientTrusted(
-                java.security.cert.X509Certificate[] certs, String authType) {
-            }
-
-            public void checkServerTrusted(
-                java.security.cert.X509Certificate[] certs, String authType) {
-            }
-        }
-    };
-
-    public String getPage(String url, List<String> params, String cookies) {
-        return getPage(url, params, cookies, 30 * 1000);
+class HttpHelper {
+    fun getPage(url: String, params: List<String>?, cookies: String?): String {
+        return getPage(url, params, cookies, 30 * 1000)
     }
 
-    public String getPageWithShortTimeout(String url, List<String> params, String cookies) {
-        return getPage(url, params, cookies, 10 * 1000);
+    fun getPageWithShortTimeout(url: String, params: List<String>?, cookies: String?): String {
+        return getPage(url, params, cookies, 10 * 1000)
     }
 
-    public String getPage(String url, List<String> params, String cookies, int timeout) {
-        String returnString;
-        StringBuilder buildString = new StringBuilder();
-
-        URLConnection connection;
+    fun getPage(url: String, params: List<String>?, cookies: String?, timeout: Int): String {
+        val returnString: String
+        val buildString = StringBuilder()
+        val connection: URLConnection
         try {
-            SSLContext sc = SSLContext.getInstance("SSL");
-            sc.init(null, trustAllCerts, null);
-
-            connection = new URL(url).openConnection();
-            if (connection instanceof HttpsURLConnection) {
-                ((HttpsURLConnection) connection).setSSLSocketFactory(sc.getSocketFactory());
+            val sc = SSLContext.getInstance("SSL")
+            sc.init(null, trustAllCerts, null)
+            connection = URL(url).openConnection()
+            if (connection is HttpsURLConnection) {
+                connection.sslSocketFactory = sc.socketFactory
             }
-            connection.setRequestProperty("User-Agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:68.0) Gecko/20100101 Firefox/68.0");
-            connection.setRequestProperty("Accept-Charset", charset);
-            connection.setRequestProperty("Accept",
-                "text/html,application/xhtml+xml,application/xml,application/json;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9");
-            connection.setConnectTimeout(timeout);
-            connection.setReadTimeout(timeout);
-
+            connection.setRequestProperty("User-Agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:68.0) Gecko/20100101 Firefox/68.0")
+            connection.setRequestProperty("Accept-Charset", charset)
+            connection.setRequestProperty(
+                "Accept",
+                "text/html,application/xhtml+xml,application/xml,application/json;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9"
+            )
+            connection.connectTimeout = timeout
+            connection.readTimeout = timeout
             if (cookies != null) {
-                connection.setRequestProperty("Cookie", cookies);
+                connection.setRequestProperty("Cookie", cookies)
             }
-
-            InputStream response = connection.getInputStream();
+            val response = connection.getInputStream()
 
 //            for (Map.Entry<String, List<String>> header : connection.getHeaderFields().entrySet()) {
 //                System.out.println(header.getKey() + "=" + header.getValue());
 //            }
-
-            String contentType = connection.getHeaderField("Content-Type");
-            String charset = null;
-
+            val contentType = connection.getHeaderField("Content-Type")
+            var charset: String? = null
             if (contentType != null) {
-                for (String param : contentType.replace(" ", "").split(";")) {
+                for (param in contentType.replace(" ", "").split(";".toRegex()).toTypedArray()) {
                     if (param.startsWith("charset=")) {
-                        charset = param.split("=", 2)[1];
-                        break;
+                        charset = param.split("=".toRegex(), 2).toTypedArray()[1]
+                        break
                     }
                 }
             }
-
             if (charset == null) {
-                charset = "UTF-8";
+                charset = "UTF-8"
             }
-
-            try (BufferedReader reader = new BufferedReader(new InputStreamReader(response, charset))) {
-                for (String line; (line = reader.readLine()) != null; ) {
-                    buildString.append(line).append(System.getProperty("line.separator"));
+            BufferedReader(InputStreamReader(response, charset)).use { reader ->
+                var line: String?
+                while (reader.readLine().also { line = it } != null) {
+                    buildString.append(line).append(System.getProperty("line.separator"))
                 }
             }
-
-        } catch (IOException | NoSuchAlgorithmException | KeyManagementException ignored) {
+        } catch (ignored: IOException) {
+        } catch (ignored: NoSuchAlgorithmException) {
+        } catch (ignored: KeyManagementException) {
         }
-
-        returnString = buildString.toString();
-
-        return returnString;
+        returnString = buildString.toString()
+        return returnString
     }
 
-    public byte[] getRawPage(String url, List<String> params, String cookies, int timeout) {
-
-        URLConnection connection;
+    fun getRawPage(url: String?, params: List<String?>?, cookies: String?, timeout: Int): ByteArray? {
+        val connection: URLConnection
         try {
-            SSLContext sc = SSLContext.getInstance("SSL");
-            sc.init(null, trustAllCerts, null);
-
-            connection = new URL(url).openConnection();
-            if (connection instanceof HttpsURLConnection) {
-                ((HttpsURLConnection) connection).setSSLSocketFactory(sc.getSocketFactory());
+            val sc = SSLContext.getInstance("SSL")
+            sc.init(null, trustAllCerts, null)
+            connection = URL(url).openConnection()
+            if (connection is HttpsURLConnection) {
+                connection.sslSocketFactory = sc.socketFactory
             }
-            connection.setRequestProperty("User-Agent",
-                "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:68.0) Gecko/20100101 Firefox/68.0");
-            connection.setRequestProperty("Accept-Charset", charset);
-            connection.setRequestProperty("Accept",
-                "text/html,application/xhtml+xml,application/xml,application/json;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9");
-            connection.setConnectTimeout(timeout);
-            connection.setReadTimeout(timeout);
-
+            connection.setRequestProperty(
+                "User-Agent",
+                "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:68.0) Gecko/20100101 Firefox/68.0"
+            )
+            connection.setRequestProperty("Accept-Charset", charset)
+            connection.setRequestProperty(
+                "Accept",
+                "text/html,application/xhtml+xml,application/xml,application/json;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9"
+            )
+            connection.connectTimeout = timeout
+            connection.readTimeout = timeout
             if (cookies != null) {
-                connection.setRequestProperty("Cookie", cookies);
+                connection.setRequestProperty("Cookie", cookies)
             }
-
-            InputStream response = connection.getInputStream();
+            val response = connection.getInputStream()
 
 //            for (Map.Entry<String, List<String>> header : connection.getHeaderFields().entrySet()) {
 //                System.out.println(header.getKey() + "=" + header.getValue());
 //            }
-
-            String contentType = connection.getHeaderField("Content-Type");
-            String charset = null;
-
+            val contentType = connection.getHeaderField("Content-Type")
+            var charset: String? = null
             if (contentType != null) {
-                for (String param : contentType.replace(" ", "").split(";")) {
+                for (param in contentType.replace(" ", "").split(";".toRegex()).toTypedArray()) {
                     if (param.startsWith("charset=")) {
-                        charset = param.split("=", 2)[1];
-                        break;
+                        charset = param.split("=".toRegex(), 2).toTypedArray()[1]
+                        break
                     }
                 }
             }
-
             if (charset == null) {
-                charset = "UTF-8";
+                charset = "UTF-8"
             }
+            return IOUtils.toByteArray(response)
+        } catch (ignored: IOException) {
+        } catch (ignored: NoSuchAlgorithmException) {
+        } catch (ignored: KeyManagementException) {
+        }
+        return null
+    }
 
-            return IOUtils.toByteArray(response);
+    fun getPage(url: String): String {
+        return getPage(url, null, null)
+    }
 
-        } catch (IOException | NoSuchAlgorithmException | KeyManagementException ignored) {
+    fun getPageWithShortTimeout(url: String): String {
+        return getPage(url, null, null, 10 * 1000)
+    }
+
+    fun getPage(url: String, timeout: Int): String {
+        return getPage(url, null, null, timeout)
+    }
+
+    fun getPage(url: String, params: List<String>?): String {
+        return getPage(url, params, null)
+    }
+
+    fun isWebsiteResponding(baseUrl: String, timeout: Int): Boolean {
+        return getPage(baseUrl, timeout).isNotEmpty()
+    }
+
+    val externalHostname: String
+        get() {
+            val builder = ProcessBuilder()
+            builder.command("bash", "-c", "dig -x $(curl -s checkip.amazonaws.com) +short")
+            builder.directory(File(System.getProperty("user.home")))
+            var output = ""
+            val error = ""
+            try {
+                val process = builder.start()
+                process.waitFor(5, TimeUnit.SECONDS)
+                output = String(process.inputStream.readAllBytes())
+            } catch (e: Exception) {
+                log.error("{}\nOutput from process:\n{}\nError from Process:\n{}", e.message, output, error)
+                e.printStackTrace()
+            }
+            return output
         }
 
-        return null;
-    }
+    companion object {
+        private const val charset = "UTF-8" // Or in Java 7 and later, use the constant: java.nio.charset.StandardCharsets.UTF_8.name()
+        private val log = LoggerFactory.getLogger(HttpHelper::class.java)
+        private val trustAllCerts = arrayOf<TrustManager>(
+            object : X509TrustManager {
+                override fun getAcceptedIssuers(): Array<X509Certificate> {
+                    return emptyArray()
+                }
 
-    public String getPage(String url) {
-        return getPage(url, null, null);
-    }
+                override fun checkClientTrusted(
+                    certs: Array<X509Certificate>,
+                    authType: String
+                ) {
+                }
 
-    public String getPageWithShortTimeout(String url) {
-        return getPage(url, null, null, 10 * 1000);
-    }
+                override fun checkServerTrusted(
+                    certs: Array<X509Certificate>,
+                    authType: String
+                ) {
+                }
+            }
+        )
 
-    public String getPage(String url, int timeout) {
-        return getPage(url, null, null, timeout);
-    }
-
-    public String getPage(String url, List<String> params) {
-        return getPage(url, params, null);
-    }
-
-    public static void downloadFileToPath(String fileURLFromTorrent, String localPath) throws IOException {
-        URL remoteUrl = new URL(fileURLFromTorrent);
-        ReadableByteChannel rbc = Channels.newChannel(remoteUrl.openStream());
-        FileOutputStream fos = new FileOutputStream(localPath);
-        fos.getChannel().transferFrom(rbc, 0, Long.MAX_VALUE);
-    }
-
-    public boolean isWebsiteResponding(String baseUrl, int timeout) {
-        return getPage(baseUrl, timeout).length() > 0;
-    }
-
-    public String getExternalHostname() {
-        ProcessBuilder builder = new ProcessBuilder();
-        builder.command("bash", "-c", "dig -x $(curl -s checkip.amazonaws.com) +short");
-        builder.directory(new File(System.getProperty("user.home")));
-        String output = "";
-        String error = "";
-        try {
-            Process process = builder.start();
-            process.waitFor(5, TimeUnit.SECONDS);
-            output = new String(process.getInputStream().readAllBytes());
-        } catch (Exception e) {
-            log.error("{}\nOutput from process:\n{}\nError from Process:\n{}", e.getMessage(), output, error);
-            e.printStackTrace();
+        @Throws(IOException::class)
+        fun downloadFileToPath(fileURLFromTorrent: String, localPath: String) {
+            val remoteUrl = URL(fileURLFromTorrent)
+            val rbc = Channels.newChannel(remoteUrl.openStream())
+            val fos = FileOutputStream(localPath)
+            fos.channel.transferFrom(rbc, 0, Long.MAX_VALUE)
         }
-        return output;
     }
 }
