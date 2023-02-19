825b159280e5872f45c8d86874587be61f9783d8
rarspace01 <rarspace01@gmail.com>
2023-02-19 17:57:01 +0100

    fix search

--- src/main/java/boat/DownloadMonitor.kt
+++ src/main/java/boat/DownloadMonitor.kt
@@ -69,7 +69,7 @@ class DownloadMonitor(
         val startOfCache = System.currentTimeMillis()
         if (multifileHosterService.isRcloneInstalled()) {
             val filesCache = cacheManager.getCache("filesCache")
-            "abcdefghijklmnopqrstuvwxyz+0".split(Regex("")).filter { it.isNotEmpty() }.map { searchName: String ->
+            "abcdefghijklmnopqrstuvwxyz+0".split(Regex("")).filter { it.isNotBlank() }.map { searchName: String ->
                 TorrentType.values()
                     .forEach { torrentType: TorrentType ->
                         val destinationPath = cloudService
